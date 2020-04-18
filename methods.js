8a081b6a75103a09a22bd9fd79c7aacd6e755e87
Miguel Gallardo <jgallardo9814@gmail.com>
2020-04-17 23:27:59 -0500

    Added combined lcm for 3 generators.

--- methods.js
+++ methods.js
@@ -61,7 +61,6 @@ const linearCongruentialMethodGenerator = (previous, a, c, m) => {
 /* Example usage */
 /* linearCongruentialMethod(4, 5, 7, 8, 8)); */
 
-
 /* Multiplicative Congruential Method */
 const multiplicativeCongruentialMethod = (a, xi, m, n) => {
   const randomNumbers = [];
@@ -70,11 +69,10 @@ const multiplicativeCongruentialMethod = (a, xi, m, n) => {
     xi = xn;
     randomNumbers.push(xn / (m - 1));
   }
-  return randomNumbers
+  return randomNumbers;
 };
 /* Example usage */
 /* multiplicativeCongruentialMethod(515, 2657, 34359738368, 100)); */
-
 
 /* Mixed Linear Congruential Method */
 
@@ -137,3 +135,41 @@ const primeFactors = (n) => {
 
 /* Example usage */
 /* mixedLinearCongruentialMethod(4, 5, 7, 8, 8)); */
+
+/* Combined Linear Congruential Method */
+
+const combinedLinearCongruentialMethod = (generators, n, lastMod) => {
+  for (let i = 0; i < 2; i++) {
+    const seed = generators[i].parameters.seed;
+    const a = generators[i].parameters.a;
+    const c = generators[i].parameters.c;
+    const m = generators[i].parameters.m;
+
+    generators[i].randomNumbers = linearCongruentialMethod(seed, a, c, m, n);
+  }
+
+  const randomNumbers = [];
+
+  for (let i = 0; i < n; i++) {
+    const r1 = generators[0].randomNumbers[i] * generators[0].parameters.m;
+    const r2 = generators[1].randomNumbers[i] * generators[1].parameters.m;
+    const r3 = r1 - r2;
+
+    if (r3 < 0) {
+      randomNumbers.push((r3 + lastMod) / lastMod);
+    } else {
+      randomNumbers.push((r3 % lastMod) / lastMod);
+    }
+  }
+
+  return randomNumbers;
+};
+
+/* Example usage */
+/* 
+const g = [
+  { parameters: { seed: 1, a: 3, c: 0, m: 5 } },
+  { parameters: { seed: 3, a: 5, c: 0, m: 7 } },
+];
+console.log(combinedLinearCongruentialMethod(g, 15, 7)); 
+*/
