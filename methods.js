a5cdd4e07cd4d03aea4f2c21c80087c01f129f2f
Miguel Gallardo <jgallardo9814@gmail.com>
2020-04-17 21:22:55 -0500

    Hull Dobell test for Mixed LC Method.

--- methods.js
+++ methods.js
@@ -60,3 +60,65 @@ const linearCongruentialMethodGenerator = (previous, a, c, m) => {
 
 /* Example usage */
 /* linearCongruentialMethod(4, 5, 7, 8, 8)); */
+
+/* Mixed Linear Congruential Method */
+
+const mixedLinearCongruentialMethod = (seed, a, c, m, n) => {
+  if (!satisfiesHullDobell(a, c, m)) {
+    return -1;
+  }
+
+  return linearCongruentialMethod(seed, a, c, m, n);
+};
+
+const satisfiesHullDobell = (a, c, m) => {
+  if (gcd(c, m) !== 1) return false;
+
+  const primeNumbers = primeFactors(m);
+  for (let i = 0; i < primeNumbers.length; i++) {
+    if ((a - 1) % primeNumbers[i] !== 0) return false;
+  }
+
+  if (m % 4 === 0) {
+    if ((a - 1) % 4 !== 0) return false;
+  }
+
+  return true;
+};
+
+const gcd = (a, b) => {
+  if (a === 0 || b === 0) return 0;
+
+  while (a !== b) {
+    if (a > b) {
+      a = a - b;
+    } else {
+      b = b - a;
+    }
+  }
+
+  return a;
+};
+
+const primeFactors = (n) => {
+  const primeNumbers = [];
+
+  while (n % 2 == 0) {
+    primeNumbers.push(2);
+    n = n / 2;
+  }
+
+  for (let i = 3; i <= Math.sqrt(n); i = i + 2) {
+    while (n % i == 0) {
+      primeNumbers.push(i);
+      n = n / i;
+    }
+  }
+
+  if (n > 2) primeNumbers.push(n);
+
+  return primeNumbers;
+};
+
+/* Example usage */
+/* mixedLinearCongruentialMethod(4, 5, 7, 8, 8)); */
